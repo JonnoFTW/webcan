cc108e31c48c0d415349409cbec1c9093e5542f6
Jonathan Mackenzie <jonmac1@gmail.com>
2018-04-18 14:57:24 +0930

    put a matplotlib import in the wrong spot, caused the server not to run it due to not having matplotlib installed

--- webcan/utils/generate_phase_co2_plots.py
+++ webcan/utils/generate_phase_co2_plots.py
@@ -1,10 +1,11 @@
-from webcan.reports import per_phase_report
-import matplotlib.pyplot as plt
-from pymongo import MongoClient
-from itertools import groupby
+
 
 
 def main():
+    from webcan.reports import per_phase_report
+    import matplotlib.pyplot as plt
+    from pymongo import MongoClient
+    from itertools import groupby
     # get all the bus data
     pass
 
